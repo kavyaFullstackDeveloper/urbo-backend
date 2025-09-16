965d51f3efc66a913440544f8b07b43e7982fde7
kavyaFullstackDeveloper <kavya.why@gmail.com>
2025-09-16 19:40:09 +0530

    Folder errors fixed

--- backend/properties/apps.py
+++ backend/properties/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+    name = 'backend.properties'
