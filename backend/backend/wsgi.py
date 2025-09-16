fc57c1af8e271ffb8677c05dfb85692e4250ad44
kavyaFullstackDeveloper <kavya.why@gmail.com>
2025-09-16 19:41:51 +0530

    Folder errors fixed

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -12,7 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.insert(0, project_root)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
